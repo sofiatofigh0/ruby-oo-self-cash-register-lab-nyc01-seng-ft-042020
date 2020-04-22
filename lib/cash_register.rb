4e34dfee2cc705e0cb6ecd4a0ed9e4310912a6fe
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-04-22 19:14:22 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,19 +3,29 @@ class CashRegister
     def initialize(discount = 0)
         @total = 0
         @discount = discount
+        @items=[]
+        @last_item=last_item
       end
 
     attr_accessor :total
     attr_accessor :discount
+    attr_accessor :items
+    attr_accessor :last_item
+    
 
-    def add_item(item_name,price,quantity=0)
+    def add_item(item_name,price,quantity=1)
         @item_name=item_name
         @price=price
         @quantity=quantity
         if quantity == 0 || quantity == nil
             self.total = total + price
         else
-            self.total = total + (price * quantity)
+            self.total = total + (price * quantity) 
+        end
+        i = quantity
+            until i == 0 do
+            @items << item_name
+             i -= 1
         end
     end
 
@@ -33,13 +43,10 @@ class CashRegister
         @items
     end
 
-    def items
-        @items=[]
-        @items << @item_name
-    end
-
-
+         
     def void_last_transaction
+        @total -= @price
+          
     end
 
 end
